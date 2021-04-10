a1050183bb362fd54f2b9375967eb0ba55177886
ChangYong <lcy9002@naver.com>
2021-04-10 10:32:37 +0900

    Initialized

---
+++ config-client/src/test/kotlin/com/yong/configclient/ConfigClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yong.configclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
